bf677221147cc591ed8ace2c7d83ee6c5f5ef9f8
Tim <tim.gagliardi.github@outlook.de>
2023-04-28 20:02:55 +0200

    assured clean logs from barotrauma and piped them into it's own file

--- src/entrypoint.sh
+++ src/entrypoint.sh
@@ -1,5 +1,7 @@
 #!/usr/bin/env bash
 
+mkdir -p /home/container/logs/
+
 echo "#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#"
 echo "Updating barotrauma"
 echo "#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#"
@@ -14,4 +16,4 @@ echo "Starting barotrauma"
 echo "#~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~~#"
 
 dos2unix "/home/container/DedicatedServer.exe"
-bash "/home/container/DedicatedServer.exe"
+bash "/home/container/DedicatedServer.exe" | sed -r "s/\x1B\[([0-9]{1,3}(;[0-9]{1,2};?)?)?[mGK]//g" | tee -a "/home/container/logs/$(date +%s).log"
